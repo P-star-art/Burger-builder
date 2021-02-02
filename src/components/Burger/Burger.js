f9355fcccc8136ee7db074cd0ffeb0c51da21bb8
Pranav <prn2108@gmail.com>
2021-02-02 12:18:59 +0530

    adding build control component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,21 @@ import classes from '../Burger/Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
-    const updatedIngredients = Object.keys(props.ingredients)
+    let updatedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])]
                 .map((_, i) => {
                     return <BurgerIngredient key={igKey + i} type={igKey} />
                 })
         })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, [])
+
+    if (updatedIngredients.length === 0) {
+        updatedIngredients = <p>Please add ingredients</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
