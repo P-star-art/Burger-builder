2782b0566c787df7c4e9d9488d6c7cbd670a8db1
Pranav <prn2108@gmail.com>
2021-02-02 22:37:38 +0530

    connecting state to build controls (added functionality to more button)

--- src/constainers/BurgerBuilder/BurgerBuilder.js
+++ src/constainers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    meat: 1.3,
+    salad: 0.5,
+    cheese: 0.4,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -10,13 +17,29 @@ class BurgerBuilder extends Component {
             salad: 0,
             cheese: 0,
             bacon: 0
-        }
+        },
+        totalPrice: 4
     }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount;
+        const additionalPrice = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + additionalPrice;
+
+        this.setState({ingredients: updatedIngredients, totalPrice: newPrice} )
+    }
+
     render() {
         return (
             <Auxiliary>
-                <Burger ingredients = {this.state.ingredients} />
-                <BuildControls />
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls ingredientAdded = {this.addIngredientHandler} />
             </Auxiliary>
         )
     }
