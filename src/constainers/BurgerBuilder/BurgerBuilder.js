c7b6174fec0a646cc67883ad6aaf0fce8d98f615
Pranav <prn2108@gmail.com>
2021-02-08 23:14:43 +0530

    sending post request of order summary to firebase database

--- src/constainers/BurgerBuilder/BurgerBuilder.js
+++ src/constainers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     meat: 1.3,
@@ -77,11 +78,29 @@ class BurgerBuilder extends Component {
     }
 
     purchaseCancelHandler = () => {
-        this.setState({purchasing: false})
+        this.setState({ purchasing: false })
     }
 
     purchaseContinueHandler = () => {
-        alert('You wanna Continue?!')
+
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Pranav',
+                address: {
+                    street: 'Daman',
+                    zipCode: '396210',
+                    country: 'India'
+                },
+                email: 'pfbf@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
     }
 
     render() {
