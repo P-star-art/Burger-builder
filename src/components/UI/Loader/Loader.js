b149e33e9c1db4268ab3a4730c4b89b2edb01b58
Pranav <prn2108@gmail.com>
2021-02-09 11:43:16 +0530

    added a loader while waiting to send POST request to firebase

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,10 @@
+import classes from '../Loader/Loader.module.css';
+import React from 'react'
+
+const Loader = (props) => {
+    return (
+        <div className={classes.Loader}>Loading...</div>
+    )
+}
+
+export default Loader;
--- src/constainers/BurgerBuilder/BurgerBuilder.js
+++ src/constainers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
+import Loader from '../../components/UI/Loader/Loader';
 
 const INGREDIENT_PRICES = {
     meat: 1.3,
@@ -23,7 +24,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     updatePurchaseHandler = (ingredients) => {
@@ -83,6 +85,8 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
 
+        this.setState({loading: true})
+
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -99,8 +103,12 @@ class BurgerBuilder extends Component {
         }
 
         axios.post('/orders.json', order)
-            .then(response => console.log(response))
-            .catch(error => console.log(error))
+            .then(response => {
+                this.setState({loading: false, purchasing: false})
+            })
+            .catch(error => { 
+                this.setState({loading: false, purchasing: false})
+            })
     }
 
     render() {
@@ -113,14 +121,20 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
 
+        let orderSummary = <OrderSummary
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+            ingredient={this.state.ingredients} />
+
+        if (this.state.loading) {
+            orderSummary = <Loader />
+        }
+
         return (
             <Auxiliary>
                 <Modal modalClosed={this.purchaseCancelHandler} show={this.state.purchasing}>
-                    <OrderSummary
-                        price={this.state.totalPrice}
-                        purchaseCancelled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler}
-                        ingredient={this.state.ingredients} />
+                    {orderSummary}
                 </Modal>
 
                 <Burger ingredients={this.state.ingredients} />
