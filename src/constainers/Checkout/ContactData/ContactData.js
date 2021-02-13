007bea5e2ee507e953674d46c40c9d29970e3312
Pranav <prn2108@gmail.com>
2021-02-13 14:57:54 +0530

    dynamically create input

--- src/constainers/Checkout/ContactData/ContactData.js
+++ src/constainers/Checkout/ContactData/ContactData.js
@@ -85,12 +85,25 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         let form = (
             <form>
-                <Input elementType="..." elementConfig="..." value="..." />
-                <Input elementType='input' type="email" name="email" placeholder="Your Email" />
-                <Input inputtype='input' type="text" name="street" placeholder="Street" />
-                <Input inputtype='input' type="text" name="postal" placeholder="Postal Code" />
+                {
+                    formElementsArray.map(formElement => {
+                        return <Input
+                            key={formElement.id}
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value}
+                        />
+                    })
+                }
                 <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
             </form>
         )
